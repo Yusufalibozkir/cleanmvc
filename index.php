a6e38a5b9a6d819eace8fba667e7d5763f2f44bd
Yusuf Ali Bozkır <yusufalibozkirbursa@gmail.com>
2013-07-08 01:09:30 +0300

    Autoloader added by YAB

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
 
 /*
- * V1 haven't got a autoloader yet. :)
+ * Set include paths for autoloader
+ */
+define('CLEAN_PATH', realpath(''));
+$paths = array(
+    CLEAN_PATH . '/config/',
+    CLEAN_PATH . '/library/',
+    get_include_path()
+);
+
+set_include_path(implode(PATH_SEPARATOR, $paths));
+
+
+/*
+ * Execute config datas
+ */
+require_once 'execute-config.php';
+
+/*
+ * Autoloader
  */
 
-require_once 'config/execute-config.php';
-require_once 'library/controller.php';
-require_once 'library/view.php';
-require_once 'boot.php';
+function __autoload($className) {
+    require_once $className . '.php';
+}
 
 $boot = new Boot($suffix);
 ?>
